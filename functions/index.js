bc37735ecf3eeae3d9158ad8ea8fd91bd333ed13
Antikillerbm <46793723+Antikillerbm@users.noreply.github.com>
2020-01-03 16:41:26 +0800

    Now function waiting for response from mqtt

--- functions/index.js
+++ functions/index.js
@@ -2,182 +2,170 @@
 
 const functions = require('firebase-functions');
 const { WebhookClient, Card, Suggestion } = require('dialogflow-fulfillment');
-var BadRequestError = require('./http-errors').BadRequestError;
 var UnauthorizedError = require('./http-errors').UnauthorizedError;
 
 var mqtt = require('mqtt');
-var formatter = new Intl.DateTimeFormat("ru", { //формат даты
 
-  year: "numeric",
-  month: "numeric",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-  timeZone: "Europe/Moscow"
-});
-
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = 'dialogflow:debug';
 
 exports.post = functions.region('europe-west1').https.onRequest((request, response) => {
-
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
   const agent = new WebhookClient({ request, response });
 
   try {
-    // Safety first. Check to see if the API key is what we configured before deploying the function
+
     if (functions.config().access.api_key != request.headers.api_key) {
       throw new UnauthorizedError("Invalid API Key - please check your configuration.");
     }
 
-    function device_switch(agent) { //наш интент
-      let devices = agent.parameters.devices.toString();
-      let status = agent.parameters.status.toString();
+    function device_switch(agent) {
 
-      var topic = 'device_switch';
-      var message = JSON.stringify({ devices, status }); //отправляем, что насобиралось в контейнер
-      agent.add(message);
-      return publishToMqtt(topic, message); //хыдыдыщ! 
+      let message = {
+        devices: agent.parameters.devices.toString(),
+        status: agent.parameters.status.toString()
+      };
+
+      message = JSON.stringify(message);
+      return publishToMqtt('device_switch', message).then((output) => {
+        agent.add(output);
+      }).catch(error => {
+        agent.add('Ожидаемой реакции сервера не последовало');
+      });
     }
 
-    function device_set(agent) { //наш интент
-      let devices = agent.parameters.devices.toString();
-      let status;
-      let color;
-      let level;
+    function device_set(agent) {
 
-      if (agent.parameters.status != null) {
-        status = agent.parameters.status.toString();
-      }
-      if (agent.parameters.level != null) {
-        level = agent.parameters.level.toString();
-      }
-      if (agent.parameters.color != null) {
-        color = agent.parameters.color.toString();
-      }
+      let message = {
+        devices: agent.parameters.devices.toString(),
+        status: agent.parameters.status.toString(),
+        color: agent.parameters.color.toString(),
+        level: agent.parameters.level.toString()
+      };
 
-      var topic = 'device_set';
-      var message = JSON.stringify({ devices, status, level, color }); //отправляем, что насобиралось в контейнер
-      agent.add(message);
-      return publishToMqtt(topic, message); //хыдыдыщ! 
+      message = JSON.stringify(message);
+      return publishToMqtt('device_set', message).then((output) => {
+        agent.add(output);
+      }).catch(error => {
+        agent.add('Ожидаемой реакции сервера не последовало');
+      });
     }
 
-    function device_time(agent) { //наш интент
-      let devices = agent.parameters.devices.toString();
-      let status = agent.parameters.status.toString();
-      let event;
-      let time;
+    function device_time(agent) { //Парсим некое значение времени и переводим все в мс
 
-      if (agent.parameters.time.toString() != '') {
-        console.log('time != null ' + agent.parameters.time.toString());//проверяем, что параметр не пустой и пихаем в стринг
-        let value = agent.parameters.time.toString().split(','); //отделяем число от единиц измерения
+      if (agent.parameters.duration.unit == "h") {
 
-        time = value[0];
-        let unit = value[1];
-
-        if (unit == "hour") {
-          console.log('time_now = ' + Date.now());
-          time = Date.now() + parseInt(time) * 3600000; //текущая дата-время + кол-во минут ожидания
-          console.log('time_milis = ' + time);
-        }
-        else if (unit == "min") {
-          time = Date.now() + arseInt(time) * 60000;
-        }
-        time = formatter.format(time).toString(); //делаем формат понятный domoticz
-        console.log('time_formatted = ' + time);
+        var duration = agent.parameters.duration.amount * 3600000;
       }
-
-      else if (agent.parameters.in_date_time.toString() != '') {
-        console.log('in_date_time != null ' + agent.parameters.in_date_time.toString());
-        time = agent.parameters.in_date_time.toString().replace('T', ' ').slice(0, 19);
-      }
-
-      else if (agent.parameters.start_date_time.startDateTime != null) {
-        console.log('start_date_time != null ' + agent.parameters.start_date_time.startDateTime.toString());
-        time = agent.parameters.start_date_time.startDateTime.toString().replace('T', ' ').slice(0, 19);
-      }
-
-      else if (agent.parameters.date_time.date_time != null) {
-        console.log('date_time != null ' + agent.parameters.date_time.date_time.toString());
-        time = agent.parameters.date_time.date_time.toString().replace('T', ' ').slice(0, 19);
+      else if (agent.parameters.duration.unit == "min") {
+        var duration = agent.parameters.duration.amount * 60000;
       }
       else {
-        agent.add('No time parameters');
-      }
+        var duration = agent.parameters.duration.amount * 1000;
+      };
 
-      if (agent.parameters.event.toString() != '') {
-        console.log('event != null ' + agent.parameters.event.toString());
-        event = agent.parameters.event.toString();
+      if (agent.parameters.date_time.date_time != null) { //Проверяем контейнер date_time, т.к. некоторые параметры имеют иную структуру 
+        var date_time = agent.parameters.date_time.date_time;
       }
       else {
-        event = ''; //событие в установленное время, если не задано иное
-      }
-      console.log('time =' + time);
-      var topic = 'device_time';
-      var message = JSON.stringify({ devices, status, time, event }); //отправляем, что насобиралось в контейнер
-      agent.add(message); //лень лезть в консоль файрбэйса, поэтому дебаг здесь))
-      return publishToMqtt(topic, message); //хыдыдыщ! 
+        date_time = agent.parameters.date_time;
+      };
+
+      let message = {
+        devices: agent.parameters.devices.toString(),
+        status: agent.parameters.status.toString(),
+        duration: duration.toString(),
+        date_time: date_time.toString()
+      };
+
+      message = JSON.stringify(message);
+      return publishToMqtt('device_time', message).then((output) => {
+        agent.add(output);
+      }).catch(error => {
+        agent.add('Ожидаемой реакции сервера не последовало');
+      });
+    }
+    function device_get(agent) {
+
+      let message = {
+        devices: agent.parameters.devices.toString(),
+        get_info: agent.parameters.get_info.toString(),
+      };
+
+      message = JSON.stringify(message);
+      return publishToMqtt('device_get', message).then((output) => {
+        agent.add(output);
+      }).catch(error => {
+        agent.add('Ожидаемой реакции сервера не последовало');
+      });
     }
 
-    // Run the proper function handler based on the matched Dialogflow intent name
-    let intentMap = new Map();
-    intentMap.set('device_set', device_set); //сюда добавляем интенты
+    let intentMap = new Map();  //Список интентов и их функции
+    intentMap.set('device_set', device_set);
     intentMap.set('device_switch', device_switch);
     intentMap.set('device_time', device_time);
+    intentMap.set('device_get', device_get);
     agent.handleRequest(intentMap);
   }
 
-  catch (err) {
+  catch (err) {  //Возвращаем ошибку, если http запрос крашнулся
     console.error(err);
     agent.add(err.message);
     agent.send_();
   }
 });
 
+function publishToMqtt(topic, message) {   //Функция отправки по mqtt
 
-function publishToMqtt(topic, message) {
-  //Options for connecting to the MQTT host
-  var options = {
-    port: functions.config().mqtt.server.port,
-    host: functions.config().mqtt.server.host,
-    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
-    username: functions.config().mqtt.server.user,
-    password: functions.config().mqtt.server.password,
-    keepalive: 60,
-    reconnectPeriod: 1000,
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clean: true,
-    encoding: 'utf8'
-  };
+  return new Promise((resolve, reject) => { //Просим подождать пока вернется сообщение
+    var options = {
+      port: functions.config().mqtt.server.port,
+      host: functions.config().mqtt.server.host,
+      clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+      username: functions.config().mqtt.server.user,
+      password: functions.config().mqtt.server.password,
+      keepalive: 60,
+      reconnectPeriod: 1000,
+      protocolId: 'MQIsdp',
+      protocolVersion: 3,
+      clean: true,
+      encoding: 'utf8'
+    };
 
-  //Let's connect
-  var client = mqtt.connect(functions.config().mqtt.server.host, options);
+    var client = mqtt.connect(functions.config().mqtt.server.host, options); //Коннектимся к брокеру
 
-  client.on('connect', function () {
-    console.log('client connected');
-  });
+    client.on('connect', function () {
+      console.log('client connected');
+    });
 
-  client.on('error', function (err) {
-    console.error(err);
-  });
+    client.on('error', function (err) {
+      console.error(err); //На случай, если соединение не удалось, завершаем ожидание
+      reject();
+    });
 
-  //debugging - check the firebase function log
-  console.log("topic: " + topic);
-  console.log("message: " + message);
+    client.subscribe('response'); //Подписка на входящие сообщения
+    client.publish(topic, message, function (err) { //Отправка
 
-  //publish the topic and payload
-  client.publish(topic, message, function (err) {
-    // handle the error
-    if (err) {
-      console.log("Error:" + err);
-      response.send("Error:" + err);
-      return;
-    }
+      if (err) {
+        console.log("Error:" + err);
+        response.send("Error:" + err);
+        reject();
+      }
 
-    //end the connection to the mqtt server
-    client.end();
+    });
+
+    client.on('message', function (topic, message) { //Ждем ответного сообщения
+      console.log("client on");
+      let output = message.toString();
+
+      resolve(output);
+      client.end();
+      clearTimeout(noResp);  //Пришло, убираем таймаут и завершаем соединение
+    });
+    let noResp = setTimeout(() => { //Если не приходит, ждем 2 секунды и говорим агенту, что все пропало
+      console.log("No connection");
+      reject();
+      client.end();
+    }, 2000)
+
   });
 }
